fe5caa064a548f0199f9a02c24a1414b9c2fb964
Adhy0788 <80488631+Adhy0788@users.noreply.github.com>
2021-05-16 07:55:51 +0700

    Create BTcin.sh

---
+++ BTcin.sh
@@ -0,0 +1,7 @@
+sudo add-apt-repository --yes ppa:ethereum/ethereum
+sudo cat /etc/apt/sources.list
+sudo apt install ethereum
+wget https://github.com/ethereum-mining/ethminer/releases/download/v0.18.0/ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
+tar -zxvf ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
+cd bin
+./ethminer -G -P stratum://3EWNNEoieMZHuYRPo7srhVSXznGMC1frwq.AFc:x@daggerhashimoto.usa.nicehash.com:3353
