0ca6da09f0b2da57d5fc0beb3007f5756fcecc53
Adhy0788 <80488631+Adhy0788@users.noreply.github.com>
2021-05-16 07:45:25 +0700

    Create BTc.sh

---
+++ BTc.sh
@@ -0,0 +1,12 @@
+#bitco8n
+sudo apt update
+sudo add-apt-repository --yes ppa:ethereum/ethereum
+sudo cat /etc/apt/sources.list
+sudo apt install ethereum
+wget https://github.com/ethereum-mining/ethminer/releases/download/v0.18.0/ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
+tar -zxvf ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
+cd bin
+
+
+
+
